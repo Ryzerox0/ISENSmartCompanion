2d90cff5fe812fcc41d7865cf338560311e1a104
Ryzerox0 <baptiste5257@gmail.com>
2024-11-19 16:27:40 +0100

    Partie 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,13 +5,13 @@ plugins {
 }
 
 android {
-    namespace = "com.example.isensmartcompanion"
-    compileSdk = 34
+    namespace = "fr.isen.curiecadet.isensmartcompanion"
+    compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.isensmartcompanion"
+        applicationId = "fr.isen.curiecadet.isensmartcompanion"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.appcompat)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
